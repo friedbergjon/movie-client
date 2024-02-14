508bd2a14c7eafe51e9fd215a48d12b70325d25a
friedbergjon <friedberg_jon@hotmail.com>
2024-02-13 20:20:57 -0600

    video player running

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ useEffect(() => {
         <Route path = "/" element={<Layout/>}>
         <Route path = "/" element={<Home movies = {movies} />}></Route>
         {/* <Route path="/Trailer/:ytTrailerId" element={<Trailer/>} /></Route> */}
-        <Route path="/Trailer/:anything" element={<Trailer />} /></Route>
+        <Route path="/Trailer/:contentId" element={<Trailer />} /></Route>
       </Routes>
     </div>
   );
